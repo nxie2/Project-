ceb5a16094d57d91086145671bdd3a75aed827a6
Mingyuan Shan <mingyuan.shan@rochester.edu>
2019-02-27 02:26:21 -0500

    indent

--- amanda.php
+++ amanda.php
@@ -40,38 +40,38 @@
     </div>
 </div>
 
-<div class="row">
-    <div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
-        <div class="card-body">
-            <h2 class="card-title">Work Life</h2>
 
-            <p class="card-text col-md-10">I’ve held a number of different jobs since beginning my college career.
-                My first research job was with the Approach-Avoidance Motivation Research Group.
-                My first summer job was working as a summer camp counselor at Mount Hope Christian School.
-                Then I worked as a teaching assistant for BIO 110, and then I worked as a teaching assistant for PSY 161.
-                Then I worked for a year at the Kid Neurolab, helping conduct fMRI research on children’s mathematical abilities.
-                Then I worked at Yale’s Computation and Cognitive Development lab for a summer.
-                This year, I’ve been working as a Peer Advisor for the Brain & Cognitive Sciences department,
-                as well as a Teaching Assistant for Machines & Consciousness.</p>
-        </div>
+<div class="card  mx-2 shadow p-0 mb-5 bg-light rounded row">
+    <div class="card-body">
+        <h2 class="card-title">Work Life</h2>
+
+        <p class="card-text col-md-10">I’ve held a number of different jobs since beginning my college career.
+            My first research job was with the Approach-Avoidance Motivation Research Group.
+            My first summer job was working as a summer camp counselor at Mount Hope Christian School.
+            Then I worked as a teaching assistant for BIO 110, and then I worked as a teaching assistant for PSY 161.
+            Then I worked for a year at the Kid Neurolab, helping conduct fMRI research on children’s mathematical abilities.
+            Then I worked at Yale’s Computation and Cognitive Development lab for a summer.
+            This year, I’ve been working as a Peer Advisor for the Brain & Cognitive Sciences department,
+            as well as a Teaching Assistant for Machines & Consciousness.</p>
     </div>
 </div>
-<div class="row">
-    <div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
-        <div class="card-body">
-            <h2 class="card-title">Free time Life</h2>
 
-            <p class="card-text col-md-10">In addition to my academic and curricular pursuits, I have a number of hobbies.
-                I really enjoy dancing, and to keep up with this interest I have completed a Dance cluster as well as been a member of
-                Ballet Performance Group. This semester, I hope to join Rochester Rangoli to learn a bit of Bollywood dance
-                I also really enjoy making different types of crafts, although I’ve had less time to pursue this activity as a college student.
-                One of my favorite types of crafts is jewelry-making. In fact, I got my ears pierced in high school to justify keeping
-                the pairs of earrings I had made and really liked. I also enjoy playing games, particularly hidden identity games.
-                Some of my recent favorites have been Avalon and Tempel des Schreckens. I also enjoy playing my clarinet,
-                indoor ice skating, and playing video games. </p>
-        </div>
+
+<div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
+    <div class="card-body">
+        <h2 class="card-title">Free time Life</h2>
+
+        <p class="card-text col-md-10">In addition to my academic and curricular pursuits, I have a number of hobbies.
+            I really enjoy dancing, and to keep up with this interest I have completed a Dance cluster as well as been a member of
+            Ballet Performance Group. This semester, I hope to join Rochester Rangoli to learn a bit of Bollywood dance
+            I also really enjoy making different types of crafts, although I’ve had less time to pursue this activity as a college student.
+            One of my favorite types of crafts is jewelry-making. In fact, I got my ears pierced in high school to justify keeping
+            the pairs of earrings I had made and really liked. I also enjoy playing games, particularly hidden identity games.
+            Some of my recent favorites have been Avalon and Tempel des Schreckens. I also enjoy playing my clarinet,
+            indoor ice skating, and playing video games. </p>
     </div>
 </div>
+
 
 
 <?php include 'inc/scripts.inc'; ?>

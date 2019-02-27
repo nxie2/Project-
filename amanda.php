8b432e73f5518994ff21c9bf4381346222d15354
Mingyuan Shan <mingyuan.shan@rochester.edu>
2019-02-27 01:48:52 -0500

    s

--- amanda.php
+++ amanda.php
@@ -20,34 +20,32 @@
 
 
 
-<div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
+<div class="card  mx-2 shadow p-0 mb-4 bg-light rounded">
     <div class="row no-gutters">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <img src="images/amanda.jpg" class="card-img" alt="Photo of Amanda">
         </div>
         <div class="col-md-8">
             <div class="card-body">
                 <h2 class="card-title">School Life</h2>
                 <p class="card-text">I am a student here at University of Rochester finishing up my major in <strong>Brain & Cognitive Sciences</strong>
-                    as well as minors in Computer Science and Psychology. I knew coming into college that I wanted to major in Brain & Cognitive Sciences because it seemed like a combination of biology and psychology—two subjects I had loved in high school.
+                    I knew coming into college that I wanted to major in Brain & Cognitive Sciences because it seemed like a combination of biology and psychology—two subjects I had loved in high school.
                     I had originally planned on double majoring in  psychology  along with my Brain & Cognitive Sciences major,
                     but when I took The Science of Programming (CSC 171) as part of my Brain & Cognitive Sciences program I loved it and decided to stick with it.
-                    I’m very glad I took that class early on in my college career so that I was able to add a Computer Science minor and explore that newfound interest.
-                    I’ve taken a large variety of different classes here at University of Rochester, and some of them were easier than others.
                     In the following paragraph I will describe some of the classes that were the most difficult for me. Artificial Intelligence was one of the most difficult classes I’ve taken at U of R,
                     because there was a lot of dense material on the exams. Basic Neurobiology was also a very difficult class because of the large amount of dense material on the exams.
-                    For each of these two classes there were 4 units, and I think each unit could have been a class all its own.</p>
+                    </p>
             </div>
         </div>
     </div>
 </div>
 
 
-<div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
+<div class="card  mx-2 shadow p-0 mb-4 bg-light rounded row">
     <div class="card-body">
     <h2 class="card-title">Work Life</h2>
 
-    <p class="card-text">I’ve held a number of different jobs since beginning my college career.
+    <p class="card-text col-md-10">I’ve held a number of different jobs since beginning my college career.
         My first research job was with the Approach-Avoidance Motivation Research Group.
         My first summer job was working as a summer camp counselor at Mount Hope Christian School.
         Then I worked as a teaching assistant for BIO 110, and then I worked as a teaching assistant for PSY 161.
@@ -57,11 +55,11 @@
         as well as a Teaching Assistant for Machines & Consciousness.</p>
     </div>
 </div>
-<div class="card  mx-2 shadow p-0 mb-5 bg-light rounded">
+<div class="card  mx-2 shadow p-0 mb-4 bg-light rounded row">
     <div class="card-body">
     <h2 class="card-title">Free time Life</h2>
 
-    <p class="card-text">In addition to my academic and curricular pursuits, I have a number of hobbies.
+    <p class="card-text col-md-10">In addition to my academic and curricular pursuits, I have a number of hobbies.
         I really enjoy dancing, and to keep up with this interest I have completed a Dance cluster as well as been a member of
         Ballet Performance Group. This semester, I hope to join Rochester Rangoli to learn a bit of Bollywood dance
         I also really enjoy making different types of crafts, although I’ve had less time to pursue this activity as a college student.

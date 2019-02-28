acd0468b3b2e8e1abd8e645b7e41220730886f63
nxie2 <47185714+nxie2@users.noreply.github.com>
2019-02-28 11:28:01 -0500

    Update and rename ding.php to combined.php

---
+++ combined.php
@@ -0,0 +1,97 @@
+<?php include 'inc/html-top.inc'; ?>
+
+
+<body class="container">
+
+
+	<header>
+        <nav class="container pt-3">
+            <ul class="nav nav-pills float-right">
+                <li class="nav-item"><a class="nav-link text-dark" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link text-white bg-danger" href="ding.php">The odd one</a></li>
+              
+            </ul>
+        </nav>
+
+    </header>    
+		<a class="text-decoration-none" href="index.php">
+			<h1>Yiqun Ding</h1>
+
+		</a>
+	
+
+    <div class="card  mx-2 shadow p-0 mb-4 bg-light rounded">
+        <div class="row no-gutters">
+            <div class="col-md-3">
+                <img src="images/ding.jpg" alt="Photo of Ding">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h2 class="card-title">School Life</h2>
+                    <p class="card-text">My original plan was to double major in <strong>Psychology</strong> and <strong>International Relation</strong>, so I took many of these major coursesduring
+                        my freshman year. However, after a year, I came to realize that International Relation is not something
+                        that I am truly interested in. Then, I started looking into other majors, and eventually picked Digital Media Studies
+                        because it covers many different fields and is related to communication, which I am considering as my job path in the
+                        future. Although there are many major classes I need to take, I am still trying to maximize what I can learn in college;
+                        I took a variety of courses such as dance, stage design, French, python, etc.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+ <a class="text-decoration-none" href="index.php">
+        <h1>Jack Dalton</h1>
+    </a>
+
+<div class="card  mx-2 shadow p-0 mb-4 bg-light rounded">
+    <div class="row no-gutters">
+        <div class="col-md-3">
+            <img src="images/jack.jpg" alt="Photo of Jack">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="card-title">School Life</h2>
+                <p class="card-text">I am a <strong>Mechanical Engineering</strong> major at the University of Rochester, and I am also minoring in Computer Science.
+                    I am very interested in how the world works, and I find both theoretical research and its application interesting.
+                    However, I decided to major in Mechanical Engineering because the idea of building something that could impact
+                    others interests me, which is why I also decided to minor in computer science. So far at UofR,
+                    I have taken the following courses: CSC 170, CSC 171, CSC 172, CSC 214, and CSC 240. Additionally,
+                    I have been a CHM 132 Lab Teacher Assistant, and I am currently a CSC 240/440 TA.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a class="text-decoration-none" href="index.php">
+        <h1>Amanda O'Donnell</h1>
+    </a>
+</header>
+
+
+
+<div class="card  mx-2 shadow p-0 mb-4 bg-light rounded">
+    <div class="row no-gutters">
+        <div class="col-md-3">
+            <img src="images/amanda.jpg" class="card-img" alt="Photo of Amanda">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="card-title">School Life</h2>
+                <p class="card-text">I am a student here at University of Rochester finishing up my major in <strong>Brain & Cognitive Sciences</strong>
+                    I knew coming into college that I wanted to major in Brain & Cognitive Sciences because it seemed like a combination of biology and psychology—two subjects I had loved in high school.
+                    I had originally planned on double majoring in  psychology  along with my Brain & Cognitive Sciences major,
+                    but when I took The Science of Programming (CSC 171) as part of my Brain & Cognitive Sciences program I loved it and decided to stick with it.
+                    In the following paragraph I will describe some of the classes that were the most difficult for me. Artificial Intelligence was one of the most difficult classes I’ve taken at U of R,
+                    because there was a lot of dense material on the exams. Basic Neurobiology was also a very difficult class because of the large amount of dense material on the exams.
+                    </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+ 	<?php include 'inc/scripts.inc'; ?>
+</body>
+
+<?php include 'inc/footer.inc'; ?>
